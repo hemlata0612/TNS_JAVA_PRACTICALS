adf5232f5c7a2a990cd6b3355e552f20b9a0295e
Hemlata Vaishnav <hemlata12062001@gmail.com>
2024-06-06 09:26:49 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,17 +1,17 @@
 public class MainActivity {
     public static void main(String[] x) {
         // Creating an Employee object and calling its methods
-        Employee emp = new Employee("Jignesh Mevada", 21, "9173127734", "123 Elm Street", 50000.0, "Software Engineering");
+        Emp e = new Emp("Hemlata", 21, "7859468791", "123 Elm Street", 50000.0, "Software Engineering");
         System.out.println("Employee Details:");
-        System.out.println("Name: " + emp.getName());
-        System.out.println("Age: " + emp.getAge());
-        System.out.println("Phone Number: " + emp.getPhoneNumber());
-        System.out.println("Address: " + emp.getAddress());
+        System.out.println("Name: " + e.getName());
+        System.out.println("Age: " + e.getAge());
+        System.out.println("Phone Number: " + e.getPhoneNumber());
+        System.out.println("Address: " + e.getAddress());
         emp.printSalary();
-        System.out.println("Specialization: " + emp.specialization);
+        System.out.println("Specialization: " + e.specialization);
 
         // Creating a Manager object and calling its methods
-        Manager mgr = new Manager("Vivek Solanki", 22, "0987654321", "456 Oak Avenue", 75000.0, "Human Resources");
+        Manager mgr = new Manager("Hir", 22, "0987654321", "456 Oak Avenue", 75000.0, "Human Resources");
         System.out.println("\nManager Details:");
         System.out.println("Name: " + mgr.getName());
         System.out.println("Age: " + mgr.getAge());
